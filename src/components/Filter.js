2f4ae3aae0247680111912a16e8186e15af407f8
Thyago Volpatto <tvolpatto@gmail.com>
2020-04-11 17:27:47 -0600

    added filter function

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,14 +1,12 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export default class Filter extends Component {
-    render() {
-        return (
-            <div className="input-group mb-3">
-                <input type="text" className="form-control" placeholder="Name or Department" aria-label="Recipient's username" aria-describedby="basic-addon2" />
-                <div className="input-group-append">
-                    <button className="btn btn-outline-secondary" type="button">Search</button>
-                </div>
-            </div>
-        )
-    }
+export default function Filter(props) {
+    return (
+        <div className="input-group mb-3">
+            <input type="text" className="form-control" placeholder="Name or Department"
+                aria-label="Recipient's username" aria-describedby="basic-addon2" 
+                onChange={props.handleInputChange} />          
+        </div>
+    )
 }
+
--- src/components/Wrapper.js
+++ src/components/Wrapper.js
@@ -3,12 +3,39 @@ import employees from "../utils/db.json"
 import Filter from './Filter'
 import Table from './Table'
 
+
+
 export default class Wrapper extends Component {
+    state = {
+        empToRender : employees
+    };
+
+    componentDidMount() {
+        this.setState(this.state);
+    }
+
+    handleClick = event => {
+        console.log(event.target)
+    }
+
+    handleInputChange = event => {
+        const value = event.target.value.toLowerCase();
+        const filteredList = employees.filter(emp => {
+            if (emp.name.toLowerCase().includes(value) 
+                || emp.department.toLowerCase().includes(value)
+                || emp.email.toLowerCase().includes(value)) {
+                return emp;
+            }
+        });
+       this.setState({empToRender : filteredList})
+
+    }
+
     render() {
         return (
             <div>
-                <Filter/>
-                <Table employees={employees}/>
+                <Filter handleInputChange={this.handleInputChange}/>
+                <Table employees={this.state.empToRender} handleClick={this.handleClick}/>
             </div>
         )
     }
