d030e283f4d5f91fd24a1e5b5ceb3c13c12c23eb
Thyago Volpatto <tvolpatto@gmail.com>
2020-04-11 19:45:26 -0600

    Updates

---
+++ precache-manifest.7ed05795d1bb5e67ec9e23c221da2a5a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cb7e0fc9d8f2beb0ce074dd36892098",
+    "url": "/employees/index.html"
+  },
+  {
+    "revision": "3a86a33cbba8ca2e93cc",
+    "url": "/employees/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b99f2993db281bc9fa91",
+    "url": "/employees/static/js/2.54d449e3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/employees/static/js/2.54d449e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a86a33cbba8ca2e93cc",
+    "url": "/employees/static/js/main.52805ddb.chunk.js"
+  },
+  {
+    "revision": "17d5ffd49ba881a1f43e",
+    "url": "/employees/static/js/runtime-main.4f92fddb.js"
+  }
+]);
