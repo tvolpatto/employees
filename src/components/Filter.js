0259fc3980986e8b028c3a27759f6847bae5cb43
Thyago Volpatto <tvolpatto@gmail.com>
2020-04-11 18:59:59 -0600

    updated the placeholder

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -3,8 +3,8 @@ import React from 'react'
 export default function Filter(props) {
     return (
         <div className="input-group mb-3">
-            <input type="text" className="form-control" placeholder="Name or Department"
-                aria-label="Recipient's username" aria-describedby="basic-addon2" 
+            <input type="text" className="form-control" placeholder="Type a name, email or department to filter"
+                aria-label="Filter option" aria-describedby="basic-addon2" 
                 onChange={props.handleInputChange} />          
         </div>
     )
